c8ef2d60f604760cbca11ddaf359973c17b9cc01
zltqzj <zltqzj@163.com>
2013-09-12 22:03:52 +0800

    Initial Commit

---
+++ jihe/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  jihe
+//
+//  Created by ZKR on 9/12/13.
+//  Copyright (c) 2013 ZKR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
